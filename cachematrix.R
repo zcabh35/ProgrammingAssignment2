4abd7b83cf8ea860bf22375d203e07cd43551826
VincentGregoryNguyen <vincent.nguyen@mezzoglobal.com>
2016-02-20 21:45:43 +0000

    Cache Matrix's
    Filled in stub methods of cachematrix.R as required by Week 3 of JHUBSPH
    course in R Programming on Cousera.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function takes in a matrix. It also uses inner methods to set and get the inverse matrix. By default the inverse matrix is null. 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) m <<- inverse
+        getInverse <- function() m
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+
+##The function tests to see if the input's inverse already exists. If it does not then it calculates it, sets it and returns the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setInverse(m)
+        m
 }
